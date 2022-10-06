a49a85f40dec1541f7e321198b73ebc450253d91
Carly Pritchard <cpritch616@gmail.com>
2022-10-06 17:01:20 -0500

    create tests for Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,17 @@
 const Manager = require("../lib/Manager");
+
+test('creates the manager object', () => {
+    const manager = new Manager('Dave', 123, 'dave@email.com', '123-456-7890');
+  
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+    expect(manager.role).toEqual('Manager');
+});
+
+test('getRole returns "manager"', () => {
+    const manager = new Manager('Dave', 123, 'dave@email.com', '123-456-7890');
+
+    expect(manager.role).toBe('Manager');
+});
