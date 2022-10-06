3006a5f1fb58835de854ffd057576e133225bf53
Carly Pritchard <cpritch616@gmail.com>
2022-10-06 16:38:27 -0500

    create passing tests for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,36 @@
 const Employee = require("../lib/Employee");
+
+test('creates the employee object', () => {
+    const employee = new Employee('Dave', 123, 'dave@email.com');
+  
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.role).toEqual('Employee');
+});
+
+test('getName returns a name', () => {
+    const employee = new Employee('Dave', 123, 'dave@email.com');
+
+    expect(employee.name).toBe('Dave');
+});
+
+test('getId returns a number', () => {
+    const employee = new Employee('Dave', 123, 'dave@email.com');
+
+    expect(employee.id).toBe('123');
+});
+
+test('getEmail returns an email', () => {
+    const employee = new Employee('Dave', 123, 'dave@email.com');
+
+    expect(employee.email).toBe('dave@email.com');
+});
+
+test('getRole returns "Employee"', () => {
+    const employee = new Employee('Dave', 123, 'dave@email.com');
+
+    expect(employee.role).toBe('Employee');
+});
+
+
