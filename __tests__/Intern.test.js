f284d66849af96fb546a0d351ea8971ce55d2a88
Carly Pritchard <cpritch616@gmail.com>
2022-10-06 16:56:30 -0500

    create tests for intern class

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,23 @@
 const Intern = require("../lib/Intern");
+
+test('creates the intern object', () => {
+    const intern = new Intern('Dave', 123, 'dave@email.com', 'College');
+  
+    expect(intern.name).toEqual(expect.any(String));
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
+    expect(intern.role).toEqual('Intern');
+});
+
+test('getSchool returns the school', () => {
+    const intern = new Intern('Dave', 123, 'dave@email.com', 'College');
+
+    expect(intern.school).toBe('College');
+});
+
+test('getRole returns "intern"', () => {
+    const intern = new Intern('Dave', 123, 'dave@email.com', 'College');
+
+    expect(intern.role).toBe('Intern');
+});
